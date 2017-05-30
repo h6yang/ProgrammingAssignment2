cddf43c3fa3c97fbefd70d9421968b625753f26c
Hai Yang <h6yang@ucsd.edu>
2017-05-29 17:55:04 -0700

    change the description

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Basicly just change example code from mean to inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -18,8 +15,6 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
